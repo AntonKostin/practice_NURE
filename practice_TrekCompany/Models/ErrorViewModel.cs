04111dd56951dfc8ad7fd0a189bc05ec7055bd90
nitka <nitka@DESKTOP-2ETQD32>
2023-05-24 20:04:52 +0300

    Add project files.

---
+++ practice_TrekCompany/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace practice_TrekCompany.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
